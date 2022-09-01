af4f9e9f94dc54de94848b7c33768bf89763c022
soongonkim <soongon@hucloud.co.kr>
2022-09-01 15:27:07 +0900

    switch event

--- app/src/main/java/com/example/coinflip/MainActivity.kt
+++ app/src/main/java/com/example/coinflip/MainActivity.kt
@@ -2,10 +2,53 @@ package com.example.coinflip
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.widget.SwitchCompat
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var simEditText: EditText
+    private lateinit var simButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val simSwitch: SwitchCompat = findViewById(R.id.main_activity_sw_simulate)
+        val flipButton: Button = findViewById(R.id.main_activity_bt_flip)
+        val resetButton: Button = findViewById(R.id.main_activity_bt_reset)
+        simButton = findViewById(R.id.main_activity_bt_simulate)
+        simEditText = findViewById(R.id.main_activity_et_simulate)
+
+        simSwitch.setOnCheckedChangeListener { compoundButton, isChecked -> enableSim(isChecked) }
+        flipButton.setOnClickListener { flip() }
+        resetButton.setOnClickListener { reset() }
+        simButton.setOnClickListener { simulate() }
     }
+
+    private fun enableSim(checked: Boolean) {
+        if (checked) {
+            simEditText.visibility = View.VISIBLE
+            simButton.visibility = View.VISIBLE
+        } else {
+            simEditText.visibility = View.INVISIBLE
+            simButton.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun flip() {
+
+    }
+
+    private fun reset() {
+
+    }
+
+    private fun simulate() {
+
+    }
+
+
 }
